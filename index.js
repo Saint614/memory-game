ff353555b560a44a78e45d18251fea10686e2034
Joshua St.Laurent <jstl35@comcast.net>
2024-03-10 12:29:13 -0400

    added remainder of my js, working on explanation comments

--- index.js
+++ index.js
@@ -27,14 +27,33 @@ function shuffleCards() {
 
 
 
-
+//flipCard functions handles the action of flipping the cards and logs the value of each card, it then ends with a call to the checkForMatch function
 function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+    if (lockBoard) return; //checks is the board is locked, if it is nothing further will be ran and will exit the function
+    if (this === firstCard) return; //checks if this is the first card in the sequence being flipped, if it is the function will end with no further code being ran
     
     this.classList.add("flipped");
+
     if (!firstCard) {
         firstCard = this;
         return;
     }
+
+    secondCard = this;
+    score++;
+    document.querySelector(".score").textContent = score;
+    lockBoard = true;
+
+    checkForMatch();
+}
+//this function compares the values of the first card logged in the flipCard function for a match
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name; //logs a boolean value of true if cards match and false if they do not
+
+  isMatch ? disableCards() : unFlipCards(); //uses ternary operator to shorten, if isMatch is true the disableCards function will be called and if it is false the unFlipCards funcion will be called
+}
+//this function pulls the cards out of play, it is called when isMatch is true in function above
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
 }
