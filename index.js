b4832d443c8b86c35747d76097228776a8cffe9e
Martha Denniss <mjdenniss@gmail.com>
2024-03-21 14:46:13 -0400

    new card, reset board, restart game, unflip card

--- index.js
+++ index.js
@@ -77,3 +77,60 @@ function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
 }
+
+//unflip card function
+const cards = document.querySelectorAll('.memory-card');
+
+  let hasFlippedCard = false;
++ let lockBoard = false;
+  let firstCard, secondCard;
+
+  function flipCard() {
++   if (lockBoard) return;
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+  }
+
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+    isMatch ? disableCards() : unflipCards();
+  }
+
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+  }
+
+  function unflipCards() {
++     lockBoard = true;
+
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
++     lockBoard = false;
+    }, 1500);
+  }
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
+
+//reset board
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+//restart game
+document.querySelector('.restart-btn').addEventListener('click', function(){
+  window.location.reload();
+  return false;
+});
